05ceab7bd297948e9792cb632a22183216eba976
Shannon Lance <shannon.lance@sap.com>
2019-12-12 14:00:18 -0800

    improved majority element

--- solved/majorityElement.py
+++ solved/majorityElement.py
@@ -8,7 +8,32 @@
 # Input: [2,2,1,1,1,2,2]
 # Output: 2
 
+# optimized solution
+# runs in O(n) for time and O(1) for space
 class Solution:
+    def majorityElement(self, nums):
+        # initiate a variable equal to the value stored at the first index of the given array
+        ans = nums[0]
+        # initiate a counter, starting at one since we already know there's at least one of the value stored in the above variable
+        count = 1
+        # iterate through the array, starting at the second index
+        for i in range(1, len(nums)):
+            # whenever the counter reaches zero
+            if count == 0:
+                # set the answer variable to the current value in the array
+                ans = nums[i]
+            # if the current value in the array is the same as the answer value, increment the counter
+            if nums[i] == ans:
+                count += 1
+            # if the current value in the array is not the same as the answer value, decrement the counter
+            if nums[i] != ans:
+                count -= 1
+        # return the final answer
+        return ans
+
+# works just fine
+# runs in O(n) for both time and space
+class mySolution:
     def majorityElement(self, nums):
         # initiate an empty dictionary
         d = {}
@@ -34,7 +59,7 @@ class Solution:
         # return the value that appears the most
         return m[0]
 
-nums = [2,2,1,1,1,2,2]
+nums = [7,7,5,7,5,1,5,7,5,5,7,7,5,5,5,5]
 s = Solution()
 a = s.majorityElement(nums)
 print(a)
